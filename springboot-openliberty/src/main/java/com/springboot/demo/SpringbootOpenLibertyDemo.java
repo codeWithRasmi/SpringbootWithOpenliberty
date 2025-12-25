1f387fa83b52a06ebd6d414122a907c2b4c13dbd
rasmiranjan <yoursolutionprovider777@gmail.com>
2025-12-25 19:19:09 +0530

    Initial commit

---
+++ springboot-openliberty/src/main/java/com/springboot/demo/SpringbootOpenLibertyDemo.java
@@ -0,0 +1,14 @@
+package com.springboot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SpringbootOpenLibertyDemo extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootOpenLibertyDemo.class, args);
+	}
+
+}
